0da878ab34d43968d26c6d35d08b1ccd7062764c
Arjun Nair <arjunnair.mec@gmail.com>
2023-08-20 01:38:06 +0200

    Pre import submodules with main module import

---
+++ timesead/__init__.py
@@ -0,0 +1,6 @@
+from . import data
+from . import evaluation
+from . import models
+from . import optim
+from . import plots
+from . import utils
--- timesead/models/__init__.py
+++ timesead/models/__init__.py
@@ -7,3 +7,11 @@ from torch.nn import Parameter
 class BaseModel(torch.nn.Module):
     def grouped_parameters(self) -> Tuple[Iterator[Parameter], ...]:
         return self.parameters(),
+
+from . import baselines
+from . import common
+from . import generative
+from . import layers
+from . import other
+from . import prediction
+from . import reconstruction
