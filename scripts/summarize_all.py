31919c6f817ddca08e815c9294ff682aa05b879b
Arjun Nair <arjunnair.mec@gmail.com>
2023-09-23 23:51:58 +0200

    Add scripts to summarize experiment results and generate an excel sheel

---
+++ scripts/summarize_all.py
@@ -0,0 +1,57 @@
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+import sys
+import glob
+import json
+import logging
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(message)s')
+
+class SheetTracker:
+    CHAR_WIDTH = 2
+    # SheetTracker for a measure stores dataset:column and experiment:row info
+    def __init__(self, wb: Workbook, measure: str):
+        self.sheet = wb.create_sheet(measure)
+        # First row/column is for labels
+        self.dataset_column_map = {'header': 1}
+        self.exp_row_map = {'header': 1}
+        self.exp_label_size = 10
+
+    def add_entry(self, exp, dataset, value):
+        if exp not in self.exp_row_map:
+            self.exp_row_map[exp] = len(self.exp_row_map) + 1
+            self.sheet.cell(self.exp_row_map[exp], 1, exp)
+            self.exp_label_size = max(self.exp_label_size, len(dataset))
+            self.sheet.column_dimensions['A'].width = self.exp_label_size * self.CHAR_WIDTH
+        if dataset not in self.dataset_column_map:
+            new_column_number = len(self.dataset_column_map) + 1
+            self.dataset_column_map[dataset] = new_column_number
+            self.sheet.cell(1, new_column_number, dataset)
+            self.sheet.column_dimensions[get_column_letter(new_column_number)].width = 5 * self.CHAR_WIDTH
+
+        self.sheet.cell(self.exp_row_map[exp], self.dataset_column_map[dataset], value)
+
+
+def process_summary_data(wb: Workbook, measure_tracker_map: dict, experiment: str, data: dict):
+    for dataset in data:
+        for measure in data[dataset]['scores']:
+            if measure not in measure_tracker_map:
+                measure_tracker_map[measure] = SheetTracker(wb, measure)
+            score, var = data[dataset]['scores'][measure]
+            measure_tracker_map[measure].add_entry(experiment, dataset, f'{score:.2f}\u00B1{var:.2f}')
+        
+
+if __name__ == '__main__':
+    wb = Workbook()
+    measure_tracker_map = dict()
+    for summary_file in glob.glob('results/*_summary.json'):
+        logging.info(f'Parsing {summary_file}')
+        with open(summary_file) as ff:
+            experiment = summary_file.split('_')[0]
+            data = json.load(ff)
+            process_summary_data(wb, measure_tracker_map, experiment, data)
+
+    output_file = 'summary.xlsx'
+    logging.info(f'Writing result to {output_file}')
+    wb.save(output_file)
+
---
+++ scripts/summarize_exp.py
@@ -0,0 +1,82 @@
+import os 
+import sys
+import glob
+import json
+import numpy as np
+import logging
+import argparse
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(message)s')
+
+def collect_dataset_results(summary: dict, dataset_dir: str, experiment: str):
+    # Read result files for the dataset, experiment and store scores in summary dict
+    scores = dict()
+    params = []
+    # For each <exp>_*/info.json in the dataset dirs
+    info_files = glob.glob(os.path.join(args.results_dir, dataset_dir, f'{experiment}_*', 'info.json'))
+    if len(info_files) < 3:
+        logging.warning(f'There should be atleast 3 runs with different seeds. Got only {len(info_files)}')
+    for info_file in info_files:
+        with open(info_file) as ff:
+            logging.debug(f'Parsing {info_file}')
+            data = json.load(ff)
+            # Collect results from "final_scores" entry
+            for metric, val in data['final_scores'].items():
+                if metric not in scores:
+                    scores[metric] = []
+                scores[metric].append(val)
+            # Collect unique best parameters from "final_best_params"
+            if data['final_best_params'] not in params:
+                params.append(data['final_best_params'])
+
+    if len(params) > 1:
+        logging.warning(f'More than one best params for {dataset_dir}')
+
+    summary[dataset_dir] = dict()
+    summary_dataset = summary[dataset_dir]
+    # Calculate mean, std of the results
+    summary_dataset['scores'] = dict()
+    for metric, vals in scores.items():
+        summary_dataset['scores'][metric] = (np.mean(vals), np.std(vals))
+    summary_dataset['params'] = params
+    logging.debug(f'Done with {dataset_dir}')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('experiment', help='Experiment name to summarize results of', type=str)
+    parser.add_argument('--results_dir', help='The directory holding the results', type=str, default='results')
+    parser.add_argument('-v', '--verbose', help='Increase output verbosity', action='store_true')
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    dataset_dirs = []
+    for entry in os.scandir(args.results_dir):
+        if not entry.is_dir():
+            continue
+        dataset_dirs.append(entry.name)
+    logging.debug(f'Found datasets: {dataset_dirs}')
+
+    # summary  dataset_dir:{scores, params}
+    summary = dict()
+    for dataset_dir in dataset_dirs:
+        try: 
+            collect_dataset_results(summary, dataset_dir, args.experiment)
+        except Exception as ee:
+            logging.error(f'Error processing results for {args.experiment} in {dataset_dir}: {ee}')
+        
+    # Write the dict as json to <exp>_summary.json
+    results_file = os.path.join(args.results_dir, f'{args.experiment}_summary.json')
+    with open(results_file, 'w') as summary_file:
+        json.dump(summary, summary_file, indent=2)
+    logging.info(f'Wrote summary to {results_file}')
+
+    # Log summary of collected scores
+    for dataset_dir, data in summary.items():
+        logging.info(f'\n=== {dataset_dir} ===')
+        for metric, vals in data['scores'].items():
+            logging.info(f'{metric}: {vals[0]:.2f}\u00B1{vals[1]:.2f}')
+
